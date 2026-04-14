420eafb270d23f58b413e3aa3d1aa07d02f4a351
Kostashchuk Iryna <kostashchuk.irina@gmail.com>
2026-04-14 23:28:27 +0300

    fix

--- src/main/java/edu/kost/opensourcekeycloaklab/DemoController.java
+++ src/main/java/edu/kost/opensourcekeycloaklab/DemoController.java
@@ -1,4 +1,4 @@
-package edu.bilak.opensourcekeycloaklab;
+package edu.kost.opensourcekeycloaklab;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
